7930b785f52551fa4a7624590e704e1bce66c58c
Rodrigo Quintas <rodrigo.quintas@savecash.com.br>
2023-01-10 14:43:31 -0300

    feat: configured postgres and implemented .env with ConfigModule from nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+      schema: process.env.DATABASE_SCHEMA,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
